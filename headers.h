c6051b4895751b92a0a8a19fb8fb582965b392c3
verma-divyanshu-git <bautocrats@gmail.com>
2024-05-01 21:01:45 +0530

    worked on basic prompt

---
+++ headers.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <stdlib.h>
+#include <grp.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+
+char cwd[1000];
+char tempHome[1000];
+char sysname[1000];
+char username[1000];
+
+
+void init_shell();
+void prompt();
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "headers.h"
+
+int main()
+{
+    init_shell();
+}
