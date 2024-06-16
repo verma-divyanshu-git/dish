f76ea6a97c980803b88fb69eef0eb2534b6f901c
verma-divyanshu-git <bautocrats@gmail.com>
2024-06-16 20:33:24 +0530

    fixed pinfo.c

---
+++ globals.c
@@ -0,0 +1,13 @@
+#include "headers.h"
+
+char cwd[100000];
+char tempHome[100000];
+char sysname[100000];
+char username[1000000];
+char *commands[100000];
+char *token[100000];
+char prevDir[100000];
+
+int bjob_count;
+bjob job_arr[100000];
+
